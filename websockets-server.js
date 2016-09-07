cf93bdf3ca3514b6b60615136007f8d6016c2c60
Joshua Tate <jmtate@icloud.com>
2016-09-06 20:01:23 -0600

    my solution to ch16 silver challenge

--- websockets-server.js
+++ websockets-server.js
@@ -5,22 +5,27 @@ var ws = new WebSocketServer({
     port: port
 });
 var messages = [];
+var PSWD = 'Swordfish';
 console.log('websockets server started');
 
 ws.on('connection', function(socket) {
     console.log('client connection established.');
-
-    messages.forEach(function(msg) {
-	socket.send(msg);
-    });
-    
+    socket.authenticated = false;
+        
     socket.on('message', function(data) {
 	console.log('message received: ' + data);
-	messages.push(data);
-	// ws object keeps track of all connections
-	// via the clients array
-	ws.clients.forEach(function(clientSocket) {
-	    clientSocket.send(data);
-	});
+	if (data === PSWD) {
+	    socket.authenticated = true;
+	    messages.forEach(function(msg) {
+		socket.send(msg);
+	    });
+	}
+	if (socket.authenticated) {
+	    messages.push(data);
+	    ws.clients.forEach(function(clientSocket) {
+		if (clientSocket.authenticated)
+		    clientSocket.send(data);
+	    });
+	}
     });
 });
