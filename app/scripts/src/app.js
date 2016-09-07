10a9418d4204b66ff7251540b1191a1f6940dac8
Joshua Tate <jmtate@icloud.com>
2016-09-07 15:45:31 -0600

    added ChatForm and ChatList

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -5,16 +5,27 @@
 */
 
 import socket from './ws-client';
+import {ChatForm, ChatList} from './dom'; // named import
+
+const FORM_SELECTOR = '[data-chat="chat-form"]';
+const INPUT_SELECTOR = '[data-chat="message-input"]';
+const LIST_SELECTOR = '[data-chat="message-list"]';
 
 class ChatApp {
     constructor() { // run any time a new instance of class is created
+	this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+	this.chatList = new ChatList(LIST_SELECTOR, 'wonderwoman');
 	socket.init('ws://localhost:3001');
 	socket.registerOpenHandler(() => {
-	    let message = new ChatMessage({ message: 'pow!' });
-	    socket.sendMessage(message.serialize());
+	    this.chatForm.init((data) => { 
+		let message = new ChatMessage({message: data});
+		socket.sendMessage(message.serialize());
+	    });
 	});
 	socket.registerMessageHandler((data) => {
 	    console.log(data);
+	    let message = new ChatMessage(data);
+	    this.chatList.drawMessage(message.serialize());
 	});
     }
 }
@@ -22,7 +33,7 @@ class ChatApp {
 class ChatMessage {
     constructor({
 	message: m,
-	user: u='batman',
+	user: u='wonderwoman',
 	timestamp: t=(new Date()).getTime()
     }) {
 	this.message = m;
